26e01502ea459908151b916d54f1bcf84bf4abc6
233moutian <1107022342@qq.com>
2018-10-09 14:58:12 +0800

    修复打包后出来的jar或war无法部署的bug。

--- src/main/java/com/vito16/shop/Application.java
+++ src/main/java/com/vito16/shop/Application.java
@@ -4,8 +4,9 @@ import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -22,7 +23,7 @@ import java.util.Properties;
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableTransactionManagement
-public class Application {
+public class Application extends SpringBootServletInitializer {
 	
 	@Value("${jdbc.driverClassName}")
 	private String driverClassName;
@@ -38,6 +39,10 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
 	}
 
 	@Bean
